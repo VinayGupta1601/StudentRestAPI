9322d400f225162592a167b576b090c4acb34b8a
Vinay Kumar <Vinay.Kumar7@tesco.com>
2023-08-26 15:03:13 +0530

    Added test case

--- src/test/java/final_spring_boot_project/service/StudentServiceTest.java
+++ src/test/java/final_spring_boot_project/service/StudentServiceTest.java
@@ -3,6 +3,7 @@ package final_spring_boot_project.service;
 import final_spring_boot_project.dao.StudentDAO;
 import final_spring_boot_project.entity.Student;
 import final_spring_boot_project.service.StudentService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -16,7 +17,9 @@ import org.springframework.test.context.ActiveProfiles;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest(classes = StudentService.class)
 public class StudentServiceTest {
@@ -31,7 +34,7 @@ public class StudentServiceTest {
 
     @BeforeEach
     public void setUp() {
-        MockitoAnnotations.initMocks(studentService);
+        //MockitoAnnotations.initMocks(studentService);
 
         student1 = Student.builder()
                 .roll(1)
@@ -46,17 +49,25 @@ public class StudentServiceTest {
                 .build();
     }
 
-    @Nested
-    class TestGetAllStudents {
+    @Test
+    public void testGetAllStudents() {
+        when(studentDAO.findAll()).thenReturn(List.of(student1, student2));
 
-        @Test
-        public void testGetAllStudents() {
-            Mockito.when(studentDAO.findAll()).thenReturn(List.of(student1, student2));
+        List<Student> output = studentService.getAllStudents();
 
-            List<Student> output = studentService.getAllStudents();
-
-            assertThat(output).containsExactlyInAnyOrderElementsOf(List.of(student1, student2));
-            verify(studentDAO).findAll();
-        }
+        assertThat(output).containsExactlyInAnyOrderElementsOf(List.of(student1, student2));
+        verify(studentDAO).findAll();
     }
+    @Test
+    public void testGetStudents() {
+        when(studentDAO.getById(1)).thenReturn(student1);
+
+        Student roll = studentService.getStudent(student1.getRoll());
+
+        assertEquals(1,student1.getRoll());
+        //assertThat(output).equals(student1.getRoll());
+
+    }
+
+
 }
